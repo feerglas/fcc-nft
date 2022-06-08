3ac561258b6fe4492545b2b511f00aa15318f498
Yves Tscherry <info@feerglas.com>
2022-06-08 15:02:40 +0200

    add logs

--- deploy/01-deploy-basic-nft.js
+++ deploy/01-deploy-basic-nft.js
@@ -6,7 +6,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  log('----------------------------------------------------');
+  log('####### deploy basic nft #######');
   arguments = [];
   const basicNft = await deploy('BasicNft', {
     from: deployer,
@@ -20,9 +20,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     !developmentChains.includes(network.name) &&
     process.env.ETHERSCAN_API_KEY
   ) {
-    log('Verifying...');
+    log('####### verify basic nft #######');
     await verify(basicNft.address, arguments);
   }
+
+  log('####### end basic nft #######');
 };
 
 module.exports.tags = ['all', 'basicnft', 'main'];
