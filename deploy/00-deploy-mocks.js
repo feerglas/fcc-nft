8a88fad9b1f509b7968444af14c9313e9281084d
Yves Tscherry <info@feerglas.com>
2022-06-08 15:02:34 +0200

    deploy price feed mock

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,8 +1,11 @@
 const { network } = require('hardhat');
 const { developmentChains } = require('../helper-hardhat-config');
 
-BASE_FEE = ethers.utils.parseEther('0.25'); // premium. costs 0.25 LINK per request
-GAS_PRICE_LINK = 1e9; // link per gas. calculated value based on the gas price of the chain
+const BASE_FEE = ethers.utils.parseEther('0.25'); // premium. costs 0.25 LINK per request
+const GAS_PRICE_LINK = 1e9; // link per gas. calculated value based on the gas price of the chain
+
+const DECIMALS = '18';
+const INITIAL_PRICE = ethers.utils.parseUnits('2000', 'ether');
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
@@ -11,11 +14,19 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
   if (developmentChains.includes(network.name)) {
     log('Local network detected. Deploying Mocks....');
+
     await deploy('VRFCoordinatorV2Mock', {
       from: deployer,
       log: true,
       args,
     });
+
+    await deploy('MockV3Aggregator', {
+      from: deployer,
+      log: true,
+      args: [DECIMALS, INITIAL_PRICE],
+    });
+
     log('Mocks deployed!');
     log('---------------------------------------------');
   }
